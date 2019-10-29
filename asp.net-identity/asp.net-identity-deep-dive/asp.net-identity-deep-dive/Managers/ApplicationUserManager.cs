ff33447f83217f1e84507d843f212eea1d203a71
Kashif Ahmed <kashif-999@outlook.com>
2019-10-29 14:53:25 +0000

    changes

--- asp.net-identity/asp.net-identity-deep-dive/asp.net-identity-deep-dive/Managers/ApplicationUserManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using asp.net.identity.deepDive.Models;
-using Microsoft.AspNetCore.Identity;
-
-namespace asp.net.identity.deepDive.Managers
-{
-    public class ApplicationUserManager : UserManager<User>
-    {
-    }
-}
--- asp.net-identity/asp.net-identity-deep-dive/asp.net-identity-deep-dive/Models/User.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace asp.net.identity.deepDive.Models
-{
-    public class User : IdentityUser
-    {
-    }
-}

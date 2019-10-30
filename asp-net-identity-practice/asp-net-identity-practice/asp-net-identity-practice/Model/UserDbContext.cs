4f244693c3ceafc684f8306b3d035d051e7a2fd6
Kashif Ahmed <kashif-999@outlook.com>
2019-10-30 08:36:20 +0000

    commit

---
+++ asp-net-identity-practice/asp-net-identity-practice/asp-net-identity-practice/Model/UserDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace asp.net.Identity.practice.Model
+{
+    public class UserDbContext : IdentityDbContext<User>
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
+
+        public DbSet<User> User { get; set; }
+    }
+}

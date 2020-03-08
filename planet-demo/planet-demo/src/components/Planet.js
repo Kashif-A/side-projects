748edc91ecf4ceea24140a9105977faca57ad944
Kashif Ahmed <kashif-999@outlook.com>
2020-03-08 20:55:28 +0000

    Initial FULL incorporation

--- planet-demo/planet-demo/src/components/Planet.js
+++ planet-demo/planet-demo/src/components/Planet.js
@@ -1,10 +1,9 @@
 import React from 'react'
 
 const Planet = (props) => 
-    <div id='mercury' className='orbit'>
+    <div id={props.planet} className='orbit'>
         <div className='pos'>
-            <div className='planet'>
-            </div>
+            <div className='planet' />
         </div>
     </div>
 

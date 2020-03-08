ab4503a267aac12d861cebb4d6ae94a575531864
Kashif Ahmed <kashif-999@outlook.com>
2020-03-08 20:25:23 +0000

    Clean up

--- planet-demo/planet-demo/src/App.js
+++ planet-demo/planet-demo/src/App.js
@@ -6,12 +6,12 @@ import './App.css';
 function App() {
   return (
     <Fragment>
-      <div className="view-3D scale-stretched">
-        <div id="solar-system">
+      <div className='view-3D scale-stretched'>
+        <div id='solar-system'>
           <Planet />
         </div>
       </div>
-      <div className="click" />
+      <div className='click' />
     </Fragment>
   )
 }
--- planet-demo/planet-demo/src/components/Planet.js
+++ planet-demo/planet-demo/src/components/Planet.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const Planet = (props) => 
-    <div id="mercury" className="orbit">
-        <div className="pos">
-            <div className="planet">
+    <div id='mercury' className='orbit'>
+        <div className='pos'>
+            <div className='planet'>
             </div>
         </div>
     </div>

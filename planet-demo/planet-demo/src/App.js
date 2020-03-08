f0c129e304403d36950a495b359cf8281973cd2e
Kashif Ahmed <kashif-999@outlook.com>
2020-03-08 20:00:34 +0000

    Removal of unnecessary files

--- planet-demo/planet-demo/src/App.js
+++ planet-demo/planet-demo/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Fragment} from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Fragment>
+      <div className="view-3D scale-stretched">
+        <div id="solar-system">
+          <div id="mercury" className="orbit">
+            <div className="pos">
+              <div className="planet">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="click" />
+    </Fragment>
+  )
 }
 
 export default App;

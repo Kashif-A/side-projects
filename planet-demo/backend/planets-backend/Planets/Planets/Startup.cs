d35ae9364ce9fb4151f43c9fcdee445bd239e5bd
Kashif Ahmed <kashif.ahmed@cantarus.com>
2020-03-09 22:39:33 +0000

    Swagger added

--- planet-demo/backend/planets-backend/Planets/Planets/Startup.cs
+++ planet-demo/backend/planets-backend/Planets/Planets/Startup.cs
@@ -44,6 +44,13 @@ namespace Planets
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MemConnect_CloudAuthConnector v1");
+                c.RoutePrefix = string.Empty;
+            });
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();

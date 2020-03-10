1b6477de13eaa5d643393cc8d45b9ffa3c876f03
Kashif Ahmed <kashif.ahmed@cantarus.com>
2020-03-10 07:57:19 +0000

    Initial full copy

--- planet-demo/planet-demo/src/components/Controls.js
+++ planet-demo/planet-demo/src/components/Controls.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
 const Controls = () =>
+<>
+  <div id='navbar'>
+      <a id='toggle-data' href='#data'><i class='icon-data'></i>Data</a>
+      <a id='toggle-controls' href='#controls'><i class='icon-controls'></i>Controls</a>
+    </div>
   <div id='data'>
     <a className='sun' title='sun' href='#sunspeed'>Sun</a>
     <a className='mercury' title='mercury' href='#mercuryspeed'>Mercury</a>
@@ -12,6 +17,27 @@ const Controls = () =>
     <a className='uranus' title='uranus' href='#uranusspeed'>Uranus</a>
     <a className='neptune' title='neptune' href='#neptunespeed'>Neptune</a>
   </div>
+  <div id='controls'>
+      <label class='set-view'>
+        <input type='checkbox' />
+      </label>
+      <label class='set-zoom'>
+        <input type='checkbox' />
+      </label>
+      <label>
+        <input type='radio' class='set-speed' name='scale' checked />
+        <span>Speed</span>
+      </label>
+      <label>
+        <input type='radio' class='set-size' name='scale' />
+        <span>Size</span>
+      </label>
+      <label>
+        <input type='radio' class='set-distance' name='scale' />
+        <span>Distance</span>
+      </label>
+    </div>
+  </>
 
   export default Controls
   

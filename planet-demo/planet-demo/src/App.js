f560f3ec3362d3232fdad03709e31a72a9ba84e8
Kashif Ahmed <kashif.ahmed@cantarus.com>
2020-03-11 20:04:34 +0000

    Clean up

--- planet-demo/planet-demo/src/App.js
+++ planet-demo/planet-demo/src/App.js
@@ -1,37 +1,37 @@
 import React, { Fragment } from 'react'
 
-import './App.css';
+import './App.css'
 
 import Planet from './components/Planet'
 import Sun from './components/Sun'
-import Controls from './components/Controls';
+import Controls from './components/Controls'
 
 function App() {
-  return (
-    <Fragment>
-      <Controls />
-      {renderUniverse()}
-    </Fragment>
-  )
-}   
+	return (
+		<Fragment>
+			<Controls />
+			{renderUniverse()}
+		</Fragment>
+	)
+}
 
 export default App
 
 const renderUniverse = () =>
-  <div id='universe' className='scale-stretched'>
-    <div id='galaxy'>
-      <div id='solar-system' className='earth'>
-        <Planet planet={'mercury'} />
-        <Planet planet={'venus'} />
-        <Planet planet={'earth'} />
-        <Planet planet={'mars'} />
-        <Planet planet={'jupiter'} />
-        <Planet planet={'saturn'} />
-        <Planet planet={'uranus'} />
-        <Planet planet={'neptune'} />
+	<div id='universe' className='scale-stretched'>
+		<div id='galaxy'>
+			<div id='solar-system' className='earth'>
+				<Planet planet={'mercury'} />
+				<Planet planet={'venus'} />
+				<Planet planet={'earth'} />
+				<Planet planet={'mars'} />
+				<Planet planet={'jupiter'} />
+				<Planet planet={'saturn'} />
+				<Planet planet={'uranus'} />
+				<Planet planet={'neptune'} />
 
-        <Sun />
+				<Sun />
 
-      </div>
-    </div>
-  </div>
+			</div>
+		</div>
+	</div>
--- planet-demo/planet-demo/src/components/Controls.js
+++ planet-demo/planet-demo/src/components/Controls.js
@@ -1,43 +1,33 @@
 import React from 'react'
 
 const Controls = () =>
-<>
-  <div id='navbar'>
-      <a id='toggle-data' href='#data'><i class='icon-data'></i>Data</a>
-      <a id='toggle-controls' href='#controls'><i class='icon-controls'></i>Controls</a>
-    </div>
-  <div id='data'>
-    <a className='sun' title='sun' href='#sunspeed'>Sun</a>
-    <a className='mercury' title='mercury' href='#mercuryspeed'>Mercury</a>
-    <a className='venus' title='venus' href='#venusspeed'>Venus</a>
-    <a className='earth active' title='earth' href='#earthspeed'>Earth</a>
-    <a className='mars' title='mars' href='#marsspeed'>Mars</a>
-    <a className='jupiter' title='jupiter' href='#jupiterspeed'>Jupiter</a>
-    <a className='saturn' title='saturn' href='#saturnspeed'>Saturn</a>
-    <a className='uranus' title='uranus' href='#uranusspeed'>Uranus</a>
-    <a className='neptune' title='neptune' href='#neptunespeed'>Neptune</a>
-  </div>
-  <div id='controls'>
-      <label class='set-view'>
-        <input type='checkbox' />
-      </label>
-      <label class='set-zoom'>
-        <input type='checkbox' />
-      </label>
-      <label>
-        <input type='radio' class='set-speed' name='scale' checked />
-        <span>Speed</span>
-      </label>
-      <label>
-        <input type='radio' class='set-size' name='scale' />
-        <span>Size</span>
-      </label>
-      <label>
-        <input type='radio' class='set-distance' name='scale' />
-        <span>Distance</span>
-      </label>
-    </div>
-  </>
+	<>
+		<div id='navbar'>
+			<a id='toggle-data' href='#data'><i className='icon-data'></i>Data</a>
+			<a id='toggle-controls' href='#controls'><i className='icon-controls'></i>Controls</a>
+		</div>
+		<div id='data'>
+			<a className='sun' title='sun' href='#sunspeed'>Sun</a>
+			<a className='mercury' title='mercury' href='#mercuryspeed'>Mercury</a>
+			<a className='venus' title='venus' href='#venusspeed'>Venus</a>
+			<a className='earth active' title='earth' href='#earthspeed'>Earth</a>
+			<a className='mars' title='mars' href='#marsspeed'>Mars</a>
+			<a className='jupiter' title='jupiter' href='#jupiterspeed'>Jupiter</a>
+			<a className='saturn' title='saturn' href='#saturnspeed'>Saturn</a>
+			<a className='uranus' title='uranus' href='#uranusspeed'>Uranus</a>
+			<a className='neptune' title='neptune' href='#neptunespeed'>Neptune</a>
+		</div>
+		<div id='controls'>
+			<label>
+				<input type='radio' className='set-speed' name='scale' checked />
+				<span>Show Speed</span>
+			</label>
+			<label>
+				<input type='radio' className='set-distance' name='scale' />
+				<span>Show Distance</span>
+			</label>
+		</div>
+	</>
 
-  export default Controls
+export default Controls
   

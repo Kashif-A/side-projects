83447909753650a63398c4bd3faa654b4089f761
Kashif Ahmed <kashif-999@outlook.com>
2020-03-13 20:13:32 +0000

    Most functionality done

--- planet-demo/planet-demo/src/App.js
+++ planet-demo/planet-demo/src/App.js
@@ -1,39 +1,27 @@
 import React, { Fragment, useState } from 'react'
 
 import './App.css'
-
-import Planet from './components/Planet'
-import Sun from './components/Sun'
 import Controls from './components/Controls'
+import Universe from './components/Universe'
+import PlanetDetail from './components/PlanetDetail'
 
 function App() {
 	const [selectedPlanet, setSelectedPlanet] = useState(null)
 	console.log(selectedPlanet)
 	return (
 		<Fragment>
+			
 			<Controls setSelectedPlanet={setSelectedPlanet} />
-			{renderUniverse()}
+			
+			<Universe />
+
+			{selectedPlanet && 
+				<PlanetDetail 
+					selectedPlanet={selectedPlanet} 
+					setSelectedPlanet={setSelectedPlanet} />}
+
 		</Fragment>
 	)
 }
 
 export default App
-
-const renderUniverse = () =>
-	<div id='universe' className='scale-stretched'>
-		<div id='galaxy'>
-			<div id='solar-system' className='earth'>
-				<Planet planet={'mercury'} />
-				<Planet planet={'venus'} />
-				<Planet planet={'earth'} />
-				<Planet planet={'mars'} />
-				<Planet planet={'jupiter'} />
-				<Planet planet={'saturn'} />
-				<Planet planet={'uranus'} />
-				<Planet planet={'neptune'} />
-
-				<Sun />
-
-			</div>
-		</div>
-	</div>

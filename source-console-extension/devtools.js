b499da066595caec1701a96c680a287d37a43b19
Kashif Ahmed <kashif-999@outlook.com>
2020-07-17 19:37:22 +0100

    changes

---
+++ source-console-extension/devtools.js
@@ -0,0 +1,8 @@
+/*var xhr = new XMLHttpRequest();
+xhr.open("GET", "/styles.css", false);
+xhr.send();
+console.log(xhr.responseText)
+chrome.devtools.panels.applyStyleSheet(xhr.responseText);
+*/
+
+console.log('here')

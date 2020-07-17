f2574cff08ce3740aa04c4521106edf0f4a05bc2
Kashif Ahmed <kashif-999@outlook.com>
2020-07-17 20:37:16 +0100

    changes

--- source-console-extension/devtools.js
+++ source-console-extension/devtools.js
@@ -1,8 +1,7 @@
-/*var xhr = new XMLHttpRequest();
-xhr.open("GET", "/styles.css", false);
-xhr.send();
-console.log(xhr.responseText)
-chrome.devtools.panels.applyStyleSheet(xhr.responseText);
-*/
-
-console.log('here')
+var x = new XMLHttpRequest();
+console.log('d')
+x.open('GET', './styles.css');
+x.send();
+x.onload = function() {
+	chrome.tabs.insertCSS(x.responseText);
+};

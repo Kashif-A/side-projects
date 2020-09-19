ab713cb243ffc15093a099f88b77681f77453bbb
Kashif Ahmed <kashif-999@outlook.com>
2020-09-19 16:37:22 +0100

    m

--- my-app/src/index.js
+++
@@ -1,5 +0,0 @@
-import ReactDOM from 'react-dom'
-import React from 'react'
-import Moment from 'react-moment'
-
-(() => setInterval(() => { ReactDOM.render(<Moment date={new Date().getTime()}/>, document.getElementById('root')) }))()

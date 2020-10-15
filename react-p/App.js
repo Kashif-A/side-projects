4377e3bd8a56e6b398c966f51ea9de1e1481383a
Kashif Ahmed <kashif-999@outlook.com>
2020-10-15 10:36:51 +0100

    m

--- react-p/App.js
+++ react-p/App.js
@@ -1,14 +1,46 @@
-import React from 'react'
-import Child1 from './src/components/Child1'
-import Child2 from './src/components/Child1'
-
-function App () {
-  return (
-    <Child1 />
-  )
+const getTodos = async (options) => {
+  const response = 'Test'
+  const data = await response
+  return data
 }
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('app')
-)
+const useGetTodos = () => {
+  const [isLoading, setIsLoading] = React.useState(false)
+  const [error, setError] = React.useState(null)
+  const [data, setData] = React.useState(null)
+  
+  const execute = async (options = {}) => {
+    try {
+      setIsLoading(true)
+      const todos = await getTodos(options)
+      setData(todos)
+      return todos
+    } catch (e) {
+      setError(e)
+      setIsLoading(false)
+      throw e
+    }
+  }
+  
+  return {
+    isLoading,
+    error,
+    data,
+    execute: React.useCallback(execute, []), // to avoid infinite calls when inside a `useEffect`
+  }
+}
+
+
+const App = () => {
+  // const { isLoading, data, execute } = useGetTodos()
+  // console.log(isLoading)
+  // console.log(data)
+  // React.useEffect(() => { try { execute() } catch(ex) {} }, [execute])
+  
+  // console.log(isLoading)
+  // console.log(data)
+  // return null
+  return 'TESTING'
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))

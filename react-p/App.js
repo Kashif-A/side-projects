059fa022f989b8dec0b51cdede72db785e2c812a
Kashif Ahmed <kashif-999@outlook.com>
2020-11-15 12:19:54 +0000

    update

--- react-p/App.js
+++ react-p/App.js
@@ -1,5 +1,14 @@
-console.log(this)
+const Child = () => {
+  const [num, setNum] = React.useState(1)
 
-const App = () => <h1>ddff</h1>
+  return (
+    <div>
+      <h1>{num}</h1>
+      <button onKeyPress={() => setNum(num + 1)} style={{ padding: 20, backgroundColor: 'red' }} />
+    </div>
+  )
+}
+
+const App = () => <Child />
 
 ReactDOM.render(<App />, document.getElementById('app'))
